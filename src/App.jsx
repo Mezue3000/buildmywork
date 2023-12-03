3cfbc14292b8d5c12dd28c6ca4f6368c31faf790
mezue30000 <mezue3000>
2023-12-03 22:22:47 +0100

    installed packages

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Jack from "./Components/Jack"
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Jack />
+       
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Jack.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Jack = () => {
+  return (
+    <h1>Jack my guy</h1>
+  )
+}
+
+export default Jack
